f11cc0a7351017b2086bc5cadc5d89440e3ae461
Celestial-Bai <60649380+Celestial-Bai@users.noreply.github.com>
2021-09-24 22:45:29 +0800

    Create IHT_config.py

--- INHERIT/IHT_config.py
+++ INHERIT/IHT_config.py
@@ -2,11 +2,12 @@
 ##### INHERIT DNABERT MODELS #####
 from basicsetting import *
 
-CONFIG_PATH = '~/transformers/dnabert-config/bert-config-6'
-BAC_TR_PATH = '~/bac_training.txt'
-PHA_TR_PATH = '~/pha_training.txt'
-BAC_VAL_PATH = '~/bac_val.txt'
-PHA_VAL_PATH = '~/pha_val.txt'
+### You should replace '.' with their paths if they do not in $Your_current_workpath/INHERIT/INHERIT ###
+CONFIG_PATH = './transformers/dnabert-config/bert-config-6'
+BAC_TR_PATH = './bac_training.txt'
+PHA_TR_PATH = './pha_training.txt'
+BAC_VAL_PATH = './bac_val.txt'
+PHA_VAL_PATH = './pha_val.txt'
 PATIENCE = 3
 KMERS = 6
 SEGMENT_LENGTH = 500
@@ -17,5 +18,5 @@ VAL_WORKERS = 6
 EPOCHS = 100
 THRESHOLD = 0.5
 LEARNING_RATE = 1e-5
-BAC_PTRMODEL = '~/bac_pretrained_model'
-PHA_PTRMODEL = '~/pha_pretrained_model'
+BAC_PTRMODEL = './bac_pretrained_model'
+PHA_PTRMODEL = './pha_pretrained_model'
